558aa357dda2756def4a0871006cba61ecbbe5bd
k0zmo <koozmoo@gmail.com>
2012-05-01 13:13:21 +0200

    * Przygotowania do BFGS

--- recalculate.m
+++ recalculate.m
@@ -1,4 +1,4 @@
-function [Q,dQ] = recalculate(stime,d,s)
+function [Q,dQ,x_,t_,u_,psi_] = recalculate(stime,d,s)
 modelparams;
 
 % stime = stime + d*s
@@ -20,18 +20,21 @@ xT = x(:,length(x));
 % wartosc wskaznika jakosci
 Q = costfun(xT);
 
-%% calkowanie rownan sprzezonych w tyl
-psiTk = R*(xf - xT); % warunek koncowy na Psi
-psi = rk4r('comodel', x, t, psiTk);
+if nargout >= 2 
+    %% calkowanie rownan sprzezonych w tyl
+    psiTk = R*(xf - xT); % warunek koncowy na Psi
+    psi = rk4r('comodel', x, t, psiTk);
 
-% figure, hold on, grid,
-% plot(t, x(1,:), 'Color', [0 0 1]);
-% plot(t, x(2,:), 'Color', [0 0.5 0]);
-% plot(t, u, 'Color', [1 0 0]);
+    % figure, hold on, grid,
+    % plot(t, x(1,:), 'Color', [0 0 1]);
+    % plot(t, x(2,:), 'Color', [0 0.5 0]);
+    % plot(t, u, 'Color', [1 0 0]);
 
-%% Pochodna wskaznika jakosci wzgledem czasow przelaczen
-dQ = costfuncderivatives(stimei, u, psi);
+    %% Pochodna wskaznika jakosci wzgledem czasow przelaczen
+    dQ = costfuncderivatives(stimei, u, psi);
+end
 
-% stime
-% Q
-% dQ
+if nargout >= 3, x_ = x; end,
+if nargout >= 4, t_ = t; end,
+if nargout >= 5, u_ = u; end,
+if nargout >= 6, psi_ = psi; end,
--- runoptim.m
+++ runoptim.m
@@ -1,8 +1,13 @@
 clc, clear;
-modelparams;
-options = optimset('GradObj','on');
-x0 = [0.5 1];
-lb = [0 0 ]';
-ub = ones(2,1)*Tk;
-[x,fval] = fmincon(@recalculate,x0,[],[],[],[],lb,ub,[],options);
 
+% options = optimset('GradObj','on');
+% x0 = [0.5 1];
+% lb = [0 0 ]';
+% ub = ones(2,1)*Tk;
+% [x,fval] = fmincon(@recalculate,x0,[],[],[],[],lb,ub,[],options);
+
+x0 = [0.5 1]; % czasy przelaczen poczatkowe
+
+[Q,dQ,x,t,u]=recalculate(x0);
+plot(t,x(1,:), t,x(2,:),t,u);
+grid on
